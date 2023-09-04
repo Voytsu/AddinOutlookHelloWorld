02353f4a95950bc81a4e4e023904078016a90232
Jordan <jordan.sautron974@gmail.com>
2023-09-04 11:36:44 +0200

    Add MessageRead

---
+++ MessageRead.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready
+        $(document).ready(function () {
+            // The document is ready
+            loadItemProps(Office.context.mailbox.item);
+            $("#btnAPI").unbind("click");
+            $("#btnAPI").click(function () {
+                //let  = 'https://assistance.azurewebsites.net/api/Priorite/GetAll';
+                //const response = await fetch(url);
+                //const myJson = await response.json(); //extract JSON from the http response
+                console.log("BtnAPI");
+            });
+        });
+    });
+
+    function loadItemProps(item) {
+        console.log("Test2");
+        // Write message property values to the task pane
+        $('#item-id').text(item.itemId);
+        $('#item-subject').text(item.subject);
+        $('#item-internetMessageId').text(item.internetMessageId);
+        $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
+    }
+});
